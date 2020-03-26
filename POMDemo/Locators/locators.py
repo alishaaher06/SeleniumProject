f7910e3e0b3255c613749f7824b107032bdcb4b4
alisha <alishaaher06@gmail.com>
2020-03-26 23:19:31 +0530

    Initial commit

---
+++ POMDemo/Locators/locators.py
@@ -0,0 +1,11 @@
+class Locators():
+
+    # Login page Locators
+    username_textbox_id = "txtUsername"
+    password_textbox_id = "txtPassword"
+    login_button_id = "btnLogin"
+
+   # Home Page Locators
+   welcome_link_id = "welcome"
+   _logout_link_linktext = "Logout"
+
